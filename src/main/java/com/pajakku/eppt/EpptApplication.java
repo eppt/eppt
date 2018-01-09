f3a8979cf42f1ffad7cc4117f56e4fb8d8b95f3e
Asep Rojali <aseprojali@gmail.com>
2018-01-09 14:26:10 +0700

    init

---
+++ src/main/java/com/pajakku/eppt/EpptApplication.java
@@ -0,0 +1,12 @@
+package com.pajakku.eppt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpptApplication.class, args);
+	}
+}
