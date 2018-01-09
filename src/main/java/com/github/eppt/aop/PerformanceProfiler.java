3a873f4616a12fb1aa4e9fb544d42221cf5b24e9
Asep Rojali <aseprojali@gmail.com>
2018-01-09 15:27:52 +0700

    add logexceution time

---
+++ src/main/java/com/github/eppt/aop/PerformanceProfiler.java
@@ -0,0 +1,25 @@
+package com.github.eppt.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+@Component
+@Aspect
+@Slf4j
+public class PerformanceProfiler {
+
+    @Around("@annotation(com.github.eppt.annotation.LogExecutionTime)")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        Object retVal = joinPoint.proceed();
+        stopWatch.stop();
+        log.info("{} took {} milliseconds.", joinPoint.toString(), stopWatch.getLastTaskTimeMillis());
+        return retVal;
+    }
+
+}
