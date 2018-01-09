d7e8efb9a0899c164fd6ee0e08e4a82387999614
Asep Rojali <aseprojali@gmail.com>
2018-01-09 14:40:57 +0700

    Add config swagger

---
+++ src/main/java/com/pajakku/eppt/config/SwaggerConfiguration.java
@@ -0,0 +1,136 @@
+package com.pajakku.eppt.config;
+
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.ApiKeyVehicle;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hibernate.validator.internal.util.CollectionHelper.newArrayList;
+
+/**
+ * Created by avew on 12/26/17.
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+
+    @Value("${swagger.rest.api}")
+    String restApi;
+
+    @Bean
+    public Docket api() throws IOException, XmlPullParserException {
+
+        List<ResponseMessage> list = new java.util.ArrayList<>();
+        list.add(new ResponseMessageBuilder().code(500).message("500 message")
+                .responseModel(new ModelRef("Result")).build());
+        list.add(new ResponseMessageBuilder().code(401).message("Unauthorized")
+                .responseModel(new ModelRef("Result")).build());
+        list.add(new ResponseMessageBuilder().code(406).message("Not Acceptable")
+                .responseModel(new ModelRef("Result")).build());
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(restApi))
+                .paths(PathSelectors.any()).build()
+                .securitySchemes(Collections.singletonList(securitySchema()))
+                .securityContexts(Collections.singletonList(securityContext()))
+                .pathMapping("/")
+                .useDefaultResponseMessages(false)
+                .apiInfo(apiInfo())
+                .globalResponseMessage(RequestMethod.GET, list)
+                .globalResponseMessage(RequestMethod.POST, list);
+
+    }
+
+    @Bean
+    springfox.documentation.swagger.web.SecurityConfiguration security() {
+        return new springfox.documentation.swagger.web.SecurityConfiguration(
+                "test-app-client-id",
+                "test-app-client-secret",
+                "test-app-realm",
+                "test-app",
+                "apiKey",
+                ApiKeyVehicle.HEADER,
+                "api_key",
+                "," /*scope separator*/);
+    }
+
+    @Bean
+    UiConfiguration uiConfig() {
+        return new UiConfiguration(
+                "validatorUrl",// url
+                "none",       // docExpansion          => none | list
+                "alpha",      // apiSorter             => alpha
+                "schema",     // defaultModelRendering => schema
+                UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS,
+                false,        // enableJsonEditor      => true | false
+                true,         // showRequestHeaders    => true | false
+                60000L);      // requestTimeout => in milliseconds, defaults to null (uses jquery xh timeout)
+    }
+
+
+    private ApiInfo apiInfo() throws IOException, XmlPullParserException {
+        MavenXpp3Reader reader = new MavenXpp3Reader();
+        Model model = reader.read(new FileReader("pom.xml"));
+        return new ApiInfoBuilder()
+                .title(model.getName())
+                .description(model.getDescription())
+                .termsOfServiceUrl("https://www.pajakku.com/tos")
+                .contact(new Contact("Asep Rojali", "http://www.aseprojali.id", "aseprojali@gmail.com"))
+                .license("license")
+                .licenseUrl("https://www.pajakku.com")
+                .version(model.getVersion()).build();
+    }
+
+    public static OAuth securitySchema() {
+
+        List<AuthorizationScope> authorizationScopeList = newArrayList();
+        authorizationScopeList.add(new AuthorizationScope("read", "read all"));
+        authorizationScopeList.add(new AuthorizationScope("write", "access all"));
+
+        List<GrantType> grantTypes = newArrayList();
+        GrantType creGrant = new ResourceOwnerPasswordCredentialsGrant("http://localhost:8000/api/uaa/oauth/token");
+
+        grantTypes.add(creGrant);
+
+        return new OAuth("oauth2schema", authorizationScopeList, grantTypes);
+
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.any())
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+
+        final AuthorizationScope[] authorizationScopes = new AuthorizationScope[2];
+        authorizationScopes[0] = new AuthorizationScope("read", "read all");
+        authorizationScopes[1] = new AuthorizationScope("write", "write all");
+
+        return Collections.singletonList(new SecurityReference("oauth2schema", authorizationScopes));
+    }
+
+}
